1569a53346e1c1c10160ce6b53ec850ff3a3c27d
Daniela Rocha e Andrade <daniela@Danielas-MacBook-Pro.local>
2022-01-21 16:05:45 +0100

    Style cleanup

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,6 @@ const port: number = 8080;
 
 app.use(express.static('./public'))
 
-app.get("/", (req: Request, res: Response) => {
-    const filePath: string = path.resolve(__dirname, './navbar-app/index.html')
-    res.sendFile(filePath)
-})
-
 app.all("*", (req: Request, res: Response) => {
     res.status(404).send('<h1>Resource not found</h1>')
 })
